cd9935fc4a6acf11827a99219142aae53673255d
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-02-24 18:18:08 -0800

    Set intial message

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => response.send("Tudo eh Lefeu!"));
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on ${port}`));
