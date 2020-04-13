dc63292c7e9ecd98a01a463410e481508d1fb505
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-04-13 16:24:26 -0700

    Create root_reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+// We will create this component shortly
+// import Root from './components/root';
+// We set this up in the last section
+import configureStore from './store/store';
 
-ReactDOM.render(<h1>Tudo eh LEFEEEEEU CARALHOOO!!</h1>, document.getElementById('root'));
+document.addEventListener('DOMContentLoaded', () => {
+    let store = configureStore({});
+
+    // Render our root component and pass in the store as a prop
+    const root = document.getElementById('root');
+
+    // ReactDOM.render(<Root store={store} />, root);
+    ReactDOM.render(<h1>Tudo eh LEFEEEEEU CARALHOOO!!</h1>, root);
+});
