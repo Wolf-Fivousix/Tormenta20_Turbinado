f6dbbf3fbd16c3a1afdf3097ebac008d31c863c1
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-04-13 16:01:31 -0700

    Clear out factory creat-react-app trash

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (request, response) => response.send("Tudo eh Lefeu!"));
+// app.get("/", (request, response) => response.send("Tudo eh Lefeu!"));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on ${port}`));
+app.listen(port, () => console.log(`Tormenta 20 Turbinado rodando na porta ${port}`));
