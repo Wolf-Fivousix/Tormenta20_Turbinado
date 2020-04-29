409124c8e05483fe5b65c52be9c74c5d4276be0a
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-04-28 17:01:35 -0700

    Force Heroku to load static build folder in production

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.use(bodyParser.json());
 
 // app.get("/", (request, response) => response.send("Tudo eh Lefeu!"));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Tormenta 20 Turbinado rodando na porta ${port}`));
