b74ace9b7d6459340e3e3a84e231c62663122ec3
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-05-01 17:59:27 -0700

    Add state changes to creature input

--- frontend/src/components/main_page.jsx
+++ frontend/src/components/main_page.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 export default function MainPage(props) {
+    const dispatch = useDispatch();
     const names = ["Wolf", "Diego", "Rex"];
-    const name = names[Math.floor(Math.random() * (names.length))];
+    const namePlaceHolder = names[Math.floor(Math.random() * names.length )];
+    const [name, setName] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // console.log(name);
+        setName("");
+    }
+
+    function handleName(e) {
+        setName(e.target.value);
+    }
 
     return (
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <h1>TUDO é Lefeu!!</h1>
-            <input type="text" placeholder={name}/>
+            <input type="text" placeholder={namePlaceHolder} onChange={handleName} value={name}/>
             <input type="text" placeholder="PV"/>
             <button>Adicionar</button>
         </form>
