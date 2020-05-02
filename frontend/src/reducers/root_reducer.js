dfefec022ae91dc51004f95ece3d6a093181bb0c
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-05-01 18:08:38 -0700

    Add creature reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
+import { creatureReducer } from "./creatureReducer";
 
-
-const RootReducer = combineReducers({
-    
+const rootReducer = combineReducers({
+    creatures: creatureReducer
 });
 
-export default RootReducer;
+export default rootReducer;
