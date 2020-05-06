d9ac7d80a63fa0e2d12b9e45427cd772db4b0f25
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-05-05 23:00:25 -0700

    Create Creature Reducer

--- frontend/src/reducers/creature_reducer.js
+++ frontend/src/reducers/creature_reducer.js
@@ -1,1 +1,16 @@
-export const creatureReducer;
+import { CREATE_CREATURE } from "../actions/creature_actions";
+
+const creatureReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch(action.type) {
+        case CREATE_CREATURE:
+            return Object.assign({}, action.creature);
+
+        default:
+            return state;
+    }
+
+};
+
+export default creatureReducer;
