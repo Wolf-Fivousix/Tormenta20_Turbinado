32c73b86e6cc0d2a58d27f733e1e528162adeed3
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-16 19:06:32 -0700

    Refactor code with shared functionality

--- frontend/src/components/main_page.jsx
+++ frontend/src/components/main_page.jsx
@@ -7,41 +7,43 @@ export default function MainPage(props) {
     const creatures = useSelector(state => state.creatures);
     const names = ["Wolf", "Diego", "Rex"];
     const namePlaceHolder = names[Math.floor(Math.random() * names.length )];
-    const [name, setName] = useState("");
-    const [pv, setPv] = useState("");
+    const name = useFormInput("");
+    const pv = useFormInput("");
+    // const [name, setName] = useState("");
+    // const [pv, setPv] = useState("");
 
     function handleSubmit(e) {
         e.preventDefault();
-        const creatureName = name || namePlaceHolder;
+        const creatureName = name.value || namePlaceHolder;
 
         const creature = {
             name: creatureName,
-            pv
+            pv: pv.value
         };
         
         dispatch(createCreature(creature))
-            .then(() => {
-                setName("");
-                setPv("");
-            });
+            // .then(() => {
+            //     setName("");
+            //     setPv("");
+            // });
     }
 
-    function handleName(e) {
-        setName(e.target.value);
-    }
+    // function handleName(e) {
+    //     setName(e.target.value);
+    // }
 
-    function handlePv(e) {
-        setPv(e.target.value);
-    }
+    // function handlePv(e) {
+    //     setPv(e.target.value);
+    // }
 
     const temporaryDisplay = Object.values(creatures).map(creature => <li>{`${creature.name}: ${creature.pv}`}</li>)
-    
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <h1>TUDO é Lefeu!!</h1>
-                <input type="text" placeholder={namePlaceHolder} onChange={handleName} value={name}/>
-                <input type="text" placeholder="PV" onChange={handlePv} value={pv}/>
+                <input type="text" placeholder={namePlaceHolder} { ...name }/>
+                <input type="text" placeholder="PV" { ...pv }/>
                 <button>Adicionar</button>
             </form>
 
@@ -51,3 +53,16 @@ export default function MainPage(props) {
         </div>
     );
 };
+
+function useFormInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    function handleValue(e) {
+        setValue(e.target.value);
+    }
+
+    return {
+        value,
+        onChange: handleValue
+    };
+}
